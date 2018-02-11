b9980a823071f30b8dc4c69576ef8f3171eae2b8
rostok <rostok@3e.pl>
2018-02-11 14:18:33 +0100

    added invert command

--- commands.js
+++ commands.js
@@ -890,6 +890,50 @@ CmdUtils.CreateCommand({
     },
 });
 
+CmdUtils.CreateCommand({
+    name: "invert",
+    description: "Inverts all colors on current page<br><br>Based on <a target=_blank href=https://stackoverflow.com/questions/4766201/javascript-invert-color-on-all-elements-of-a-page>this</a>.",
+    execute: function execute(){
+        chrome.tabs.executeScript({code:`
+        javascript: (
+            function () { 
+            // the css we are going to inject
+            var css = 'html {-webkit-filter: invert(100%);' +
+                '-moz-filter: invert(100%);' + 
+                '-o-filter: invert(100%);' + 
+                '-ms-filter: invert(100%); }',
+            
+            head = document.getElementsByTagName('head')[0],
+            style = document.createElement('style');
+            
+            // a hack, so you can "invert back" clicking the bookmarklet again
+            if (!window.counter) { window.counter = 1;} else  { window.counter ++;
+            if (window.counter % 2 == 0) { var css ='html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }'}
+             };
+            
+            style.type = 'text/css';
+            if (style.styleSheet){
+            style.styleSheet.cssText = css;
+            } else {
+            style.appendChild(document.createTextNode(css));
+            }
+            
+            //injecting the css to the head
+            head.appendChild(style);
+
+            function invert(rgb) {
+                rgb = Array.prototype.join.call(arguments).match(/(-?[0-9\.]+)/g);
+                for (var i = 0; i < rgb.length; i++) {
+                  rgb[i] = (i === 3 ? 1 : 255) - rgb[i];
+                }
+                return rgb;
+            }
+
+            document.body.style.backgroundColor = "rgb("+invert(window.getComputedStyle(document.body, null).getPropertyValue('background-color')).join(",")+")";
+            }());
+        `})
+    },
+});
 // - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  - 
 // - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  - 
 // - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  -  - -  - 
