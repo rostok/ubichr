cdc5912592c8385b43c4e70164107c56a5089353
rostok <rostok@3e.pl>
2018-02-19 17:42:04 +0100

    added protocol switch for separate window debugging

--- core.js
+++ core.js
@@ -1,5 +1,32 @@
 // common script for UI pages that initializes global objects
 
-var backgroundPage = chrome.extension.getBackgroundPage();
+var backgroundPage;
+switch(window.location.protocol) {
+   case 'http:':
+   case 'https:':
+   case 'file:':	// for local file debugging
+   					backgroundPage = window;
+   					var head = document.getElementsByTagName("head")[0];
+					var s;
+					s = document.createElement("script");
+					s.type = "text/javascript";
+					s.src = "utils.js";
+					head.appendChild(s);
+					s = document.createElement("script");
+					s.type = "text/javascript";
+					s.src = "cmdutils.js";
+					head.appendChild(s);
+					s = document.createElement("script");
+					s.type = "text/javascript";
+					s.src = "commands.js";
+					head.appendChild(s);
+     				break;
+
+   case 'chrome-extension:':
+   default: 
+     				backgroundPage = chrome.extension.getBackgroundPage();
+}
+
+//var backgroundPage = chrome.extension.getBackgroundPage();
 var Utils = backgroundPage.Utils;
 var CmdUtils = backgroundPage.CmdUtils;
